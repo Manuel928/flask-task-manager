f5181cf08304f1e3a28a047d9ed388f63e3f396c
Emmanuel Oluwatosin Alao <Afrikareportersng@gmail.com>
2024-11-28 18:47:34 +0100

    Updated database(supabase)

--- app.py
+++ app.py
@@ -1,119 +1,82 @@
-from flask import Flask, render_template, request, redirect
-import sqlite3
 from datetime import datetime
+from flask import Flask, request, render_template, jsonify, redirect, url_for, flash
+from dotenv import load_dotenv
+import os
+from supabase import create_client, Client
+
+load_dotenv()
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 app = Flask(__name__)
+supabase: Client = create_client(url, key)
 
 
-def init_db():
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-    cursor.execute("""
-            CREATE TABLE IF NOT EXISTS tasks (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    title TEXT NOT NULL,
-                    priority TEXT NOT NULL,
-                    status TEXT DEFAULT 'Pending',
-                    due_date_and_time DATETIME NOT NULL
-            )""")
-    conn.commit()
-    conn.close()
-
-
-init_db()
-
-
-@app.route("/")
+@app.route('/')
 def home():
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-    cursor.execute(" SELECT * FROM tasks ")
-    tasks = cursor.fetchall()
-    conn.close()
-    formatted_tasks = [
-        {
-            "id": task[0],
-            "title": task[1],
-            "priority": task[2],
-            "status": task[3],
-            "due_date_and_time": task[4],
-        }
-        for task in tasks
-    ]
-    return render_template("index.html", tasks=formatted_tasks)
+    response = supabase.table('tasks').select(
+        '*').order('id', desc=False).execute()
+    return render_template('index.html', tasks=response.data)
 
 
-@app.route("/add-task")
+@app.route('/add-task')
 def add_task():
-    return render_template("add_task.html")
+    return render_template('add_task.html')
 
 
-@app.route("/submit-task", methods=["POST"])
+@app.route('/submit-task', methods=['POST'])
 def submit_task():
-    title = request.form["title"]
-    priority = request.form["priority"]
-    status = "Pending"
-    due_date_and_time_str = request.form["due_date_and_time"]
-    due_date_and_time = datetime.strptime(
-        due_date_and_time_str, "%Y-%m-%dT%H:%M")
+    title = request.form.get('title')
+    priority = request.form.get('priority')
+    status = request.form.get('status')
+    due_date_and_time = request.form.get('due_date_and_time')
 
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-    cursor.execute(" INSERT INTO tasks (title, priority, status, due_date_and_time) VALUES (?, ?, ?, ?)",
-                   (title, priority, status, due_date_and_time))
-    conn.commit()
-    conn.close()
-
-    return redirect("/")
+    task_data = {
+        'title': title,
+        'priority': priority,
+        'status': status,
+        'due_date_and_time': due_date_and_time
+    }
+    supabase.table('tasks').insert(task_data).execute()
+    return redirect('/')
 
 
-@app.route("/edit_task/<int:id>", methods=["GET", "POST"])
+@app.route('/edit_task/<int:id>', methods=['GET', 'POST'])
 def edit_task(id):
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-
-    if request.method == "GET":
-        cursor.execute(" SELECT * FROM tasks WHERE id = ? ", (id,))
-        data = cursor.fetchone()
-        conn.close()
-
-        if data:
-            return render_template("edit_task.html", data=data)
+    if request.method == 'GET':
+        response = supabase.table('tasks').select('*').eq('id', id).execute()
+        if response.data:
+            # print(response.data)
+            return render_template('edit_task.html', data=response.data)
         else:
-            return "Entry Not Found", 404
-    elif request.method == "POST":
-        title = request.form["title"]
-        priority = request.form["priority"]
-        status = request.form["status"]
-        due_date_and_time_str = request.form["due_date_and_time"]
-        due_date_and_time = datetime.strptime(
-            due_date_and_time_str, "%Y-%m-%dT%H:%M")
-        cursor.execute(" UPDATE tasks SET title = ?, priority = ?, status = ?, due_date_and_time = ? WHERE id = ? ",
-                       (title, priority, status, due_date_and_time, id))
-        conn.commit()
-        conn.close()
+            return "Not Found", 404
+    elif request.method == 'POST':
+        title = request.form.get('title')
+        priority = request.form.get('priority')
+        status = request.form.get('status')
+        due_date_and_time = request.form.get('due_date_and_time')
 
-        return redirect("/")
+        task_data = {
+            'title': title,
+            'priority': priority,
+            'status': status,
+            'due_date_and_time': due_date_and_time
+        }
+        supabase.table('tasks').update(task_data).eq('id', id).execute()
+    return redirect('/')
+
+
+@app.route("/complete/<int:id>")
+def mark_as_complete(id):
+    supabase.table('tasks').update(
+        {"status": "Completed"}).eq('id', id).execute()
+    return redirect("/")
 
 
 @app.route("/delete_task/<int:id>")
 def delete_task(id):
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-    cursor.execute(" DELETE FROM tasks WHERE id = ?", (id,))
-    conn.commit()
-    conn.close()
-
-    return redirect("/")
-
-@app.route("/complete/<int:id>")
-def mark_as_complete(id):
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-    cursor.execute(
-        " UPDATE tasks SET status = 'Completed' WHERE id = ?", (id,))
-    conn.commit()
-    conn.close()
+    supabase.table('tasks').delete().eq('id', id).execute()
     return redirect("/")
 
 
